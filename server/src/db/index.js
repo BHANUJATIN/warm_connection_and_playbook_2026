1972060dca41819852acf6a3eefbabc8005c2fb1
BHANUJATIN <bjsbainsla@gmail.com>
2026-02-06 16:45:50 +0530

    m

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -3,7 +3,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: 'postgresql://postgres.ukislwzdnjpwxfkggggl:mXYjKf5Sz0FcJaV0@aws-1-eu-central-1.pooler.supabase.com:6543/postgres'
+  connectionString: process.env.DATABASE_URL
 
 });
 console.log(process.env.DATABASE_URL);
